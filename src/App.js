af76569a1c2a1736814e1a6083476a50a8586807
Jesse Pollack <jesse.lincolndouglas@gmail.com>
2020-11-13 08:32:51 -0500

    committing w vercel

--- src/App.js
+++ src/App.js
@@ -1,96 +1,84 @@
-import React, { Component } from 'react';
-import AddBookmark from './AddBookmark/AddBookmark';
-import BookmarkList from './BookmarkList/BookmarkList';
-import Nav from './Nav/Nav';
-import config from './config';
-import './App.css';
+import React, { Component } from "react";
+import AddBookmark from "./AddBookmark/AddBookmark";
+import BookmarkList from "./BookmarkList/BookmarkList";
+import Nav from "./Nav/Nav";
+import config from "./config";
+import "./App.css";
+import { Route } from "react-router-dom";
+import BookmarksContext from './BookmarksContext/BookmarksContext';
+import Rating from './Rating/Rating'
 
-const bookmarks = [
-  // {
-  //   id: 0,
-  //   title: 'Google',
-  //   url: 'http://www.google.com',
-  //   rating: '3',
-  //   desc: 'Internet-related services and products.'
-  // },
-  // {
-  //   id: 1,
-  //   title: 'Thinkful',
-  //   url: 'http://www.thinkful.com',
-  //   rating: '5',
-  //   desc: '1-on-1 learning to accelerate your way to a new high-growth tech career!'
-  // },
-  // {
-  //   id: 2,
-  //   title: 'Github',
-  //   url: 'http://www.github.com',
-  //   rating: '4',
-  //   desc: 'brings together the world\'s largest community of developers.'
-  // }
-];
+
 
 class App extends Component {
   state = {
-    page: 'list',
-    bookmarks,
+    bookmarks: [],
     error: null,
   };
-
-  changePage = (page) => {
-    this.setState({ page })
-  }
-
-  setBookmarks = bookmarks => {
+  deleteBookmark = bookmarkId => {
+        const newBookmarks = this.state.bookmarks.filter(bm =>
+          bm.id !== bookmarkId
+        )
+      this.setState({
+         bookmarks: newBookmarks
+      })
+      }
+  setBookmarks = (bookmarks) => {
     this.setState({
       bookmarks,
       error: null,
-      page: 'list',
-    })
-  }
+      page: "list",
+    });
+  };
 
-  addBookmark = bookmark => {
+  addBookmark = (bookmark) => {
     this.setState({
-      bookmarks: [ ...this.state.bookmarks, bookmark ],
-    })
-  }
+      bookmarks: [...this.state.bookmarks, bookmark],
+    });
+  };
 
   componentDidMount() {
     fetch(config.API_ENDPOINT, {
-      method: 'GET',
+      method: "GET",
       headers: {
-        'content-type': 'application/json',
-        'Authorization': `Bearer ${config.API_KEY}`
-      }
+        "content-type": "application/json",
+        Authorization: `Bearer ${config.API_KEY}`,
+      },
     })
-      .then(res => {
+      .then((res) => {
         if (!res.ok) {
-          throw new Error(res.status)
+          throw new Error(res.status);
         }
-        return res.json()
+        return res.json();
       })
       .then(this.setBookmarks)
-      .catch(error => this.setState({ error }))
+      .catch((error) => this.setState({ error }));
   }
 
   render() {
-    const { page, bookmarks } = this.state
+    const contextValue = {
+             bookmarks: this.state.bookmarks,
+             addBookmark: this.addBookmark,
+             deleteBookmark: this.deleteBookmark,
+           }
     return (
-      <main className='App'>
+      <main className="App">
         <h1>Bookmarks!</h1>
-        <Nav clickPage={this.changePage} />
-        <div className='content' aria-live='polite'>
-          {page === 'add' && (
-            <AddBookmark
-              onAddBookmark={this.addBookmark}
-              onClickCancel={() => this.changePage('list')}
-            />
-          )}
-          {page === 'list' && (
-            <BookmarkList
-              bookmarks={bookmarks}
-            />
-          )}
-        </div>
+        <Rating value="hello"/>
+        <BookmarksContext.Provider value={contextValue}>
+           <Nav />
+           <div className='content' aria-live='polite'>
+          <Route
+            path="/add-bookmark"
+            component={AddBookmark}
+          />
+          <Route
+            exact
+            path="/"
+            component={BookmarkList}
+          />
+          </div>
+         </BookmarksContext.Provider>
       </main>
     );
   }
--- src/BookmarkList/BookmarkList.js
+++ src/BookmarkList/BookmarkList.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
 import BookmarkItem from '../BookmarkItem/BookmarkItem';
 import './BookmarkList.css'
+import BookmarksContext from '../BookmarksContext/BookmarksContext'
+import PropTypes from 'prop-types';
 
 class BookmarkList extends Component {
   static defaultProps = {
     bookmarks: []
   };
-
+  static contextType = BookmarksContext;
   render() {
-    const { bookmarks } = this.props
+    BookmarkList.propTypes = {
+      bookmarks: PropTypes.arrayOf(PropTypes.shape({
+        title: PropTypes.string.isRequired,
+        url: PropTypes.string.isRequired,
+        rating: PropTypes.number,
+        description: PropTypes.string
+      }))
+    };
+    const { bookmarks } = this.context
     return (
       <section className='BookmarkList'>
         <h2>Your bookmarks</h2>
